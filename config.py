4dafa3363564382aa126776b9ffcc7bd6e3132a4
arshturk839 <arshgaming.1011@gmail.com>
2025-07-11 09:43:42 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TELEGRAM_TOKEN = "8184952662:AAEgnbz-vwQwJTG6vQ_qKdMUXUuU4FIMLzE"
+
+CREATE_ID_URL = "https://hotexch10.com/api/create-id"
+DEPOSIT_URL = "https://hotexch10.com"
+WITHDRAW_URL = "https://hotexch10.com"
+BALANCE_URL = "https://hotexch.com"
+
+STATIC_UPI_ID = "9917241743-3@axl"
